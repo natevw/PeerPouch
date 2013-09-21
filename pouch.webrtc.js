999c4780a2a0affbd751df9c0937dde350ef8cf0
Nathan Vander Wilt <nate@calftrail.com>
2013-09-21 14:55:49 -0700

    closer to closures

--- pouch.webrtc.js
+++ pouch.webrtc.js
@@ -20,11 +20,6 @@
     - hide all connections setup, expose only via adapter (for connecting) and plugin (for sharing)
 */
 
-function call(cb) {     // TODO: remove use of this old helper!
-    // based on https://github.com/daleharvey/pouchdb/blob/master/src/pouch.utils.js#L52
-    if (cb) cb.apply(this, Array.prototype.slice.call(arguments, 1));
-}
-
 // a couple additional errors we use
 Pouch.Errors.NOT_IMPLEMENTED = {status:501, error:'not_implemented', reason:"Unable to fulfill the request"};       // [really for METHODs only?]
 Pouch.Errors.FORBIDDEN = {status:403, error:'forbidden', reason:"The request was refused"};
@@ -34,8 +29,11 @@ Pouch.Errors.FORBIDDEN = {status:403, error:'forbidden', reason:"The request was
 var PeerPouch = function(opts, callback) {
     function TODO(callback) {
         // TODO: callers of this function want implementations
-        setTimeout(function () { call(callback, Pouch.Errors.NOT_IMPLEMENTED); }, 0);
+        if (callback) setTimeout(function () { callback(Pouch.Errors.NOT_IMPLEMENTED); }, 0);
     }
+    
+    console.log("PEERPOUCH?", PeerPouch._sharePouches[opts.name]);
+    
     
     // expect we'll need: basic identity for available peers, share per-peer connections between db instances
     
@@ -59,7 +57,7 @@ var PeerPouch = function(opts, callback) {
     
     // TODO: update this with latest (e.g. _getRevisionTree, _removeDocRevisions)
     
-    // these should be implemented as very generic simple RPC-type stuff (client here and server in PeerPouch.Presence)
+    // these should be implemented as very generic simple RPC-type stuff
     // for now don't allow *any* remote code execution — eventually optimize map/reduce to happen in WebWorker but, until then,
     // instead use dNode-style (IIRC) trick of serializing functions as an ID and then executing back locally when called on remote
     // (how to handle synchronous map/reduce functions?)
@@ -101,7 +99,7 @@ var PeerPouch = function(opts, callback) {
 
 // Don't bother letting peers nuke each others' databases
 PeerPouch.destroy = function(name, callback) {
-    setTimeout(function () { call(callback, Pouch.Errors.FORBIDDEN); }, 0);
+    if (callback) setTimeout(function () { callback(Pouch.Errors.FORBIDDEN); }, 0);
 };
 
 // Can we breathe in this environment?
@@ -114,361 +112,9 @@ PeerPouch.valid = function() {
 PeerPouch._types = {
     presence: 'com.stemstorage.peerpouch.presence',
     signal: 'com.stemstorage.peerpouch.signal',
-    share: 'com.stemstorage.peerpouch.share',
-    ddoc_name: 'peerpouch-dev'
+    share: 'com.stemstorage.peerpouch.share'
 }
-
 var _t = PeerPouch._types;         // local alias for brevitation…
-function _ddoc_replacer(k,v) {
-    return (typeof v === 'function') ? v.toString().replace(/_t.(\w+)/, function (m,t) {        // …and hacky unbrevitation
-        return JSON.stringify(_t[t]);
-    }) : v;
-}
-function _jsonclone(d, r) {
-    //return JSON.parse(JSON.stringify(d,r));
-    
-    // WORKAROUND: https://code.google.com/p/chromium/issues/detail?id=222982#makechanges
-    if (r) {
-        var toJSON = function (k, d) {
-            d = r(k, d);
-            if (typeof d === 'object') Object.keys(d).forEach(function (k) {
-                d[k] = toJSON(k,d[k]);
-            });
-            return d;
-        }
-        d = toJSON(null, d);
-    }
-    return JSON.parse(JSON.stringify(d));
-}
-
-
-PeerPouch._ddoc = _jsonclone({
-    _id: '_design/' + _t.ddoc_name,
-    filters: {
-        signalling: function (doc, req) {
-            return (doc[_t.presence] || (doc[_t.signal] && doc.recipient === req.query.identity));
-        }
-    },
-    views: {
-        peers_by_identity: {
-            map: function (doc) { if (doc[_t.presence]) emit(doc.identity, doc.name); }
-        }
-    }
-}, _ddoc_replacer);
-
-PeerPouch.Presence = function(hub, opts, cb) {
-    if (typeof opts === 'function') {
-        cb = opts;
-        opts = {};
-    }
-    opts || (opts == {});
-    
-    // hub is *another* Pouch instance (typically http type) — we'll use that database for communicating presence/offers/answers!
-    // opts includes: name string, identity string/TBD, profile object, share {name:db}, peerUpdate callback
-    // api allows: getPeers(), connectTo(), disconnect()
-    
-    // TODO: add concept of "separate" peer groups within a common hub?
-    
-    var RTCPeerConnection = window.RTCPeerConnection || webkitRTCPeerConnection || mozRTCPeerConnection,
-        RTCSessionDescription = window.RTCSessionDescription || webkitRTCSessionDescription || mozRTCSessionDescription,
-        RTCIceCandidate = window.RTCIceCandidate || webkitRTCIceCandidate || mozRTCIceCandidate;
-    
-    // TODO: make ICE (and other channel setup params?) user-configurable
-    var cfg = {"iceServers":[{"url":"stun:23.21.150.121"}]},
-        con = { 'optional': [{'DtlsSrtpKeyAgreement': true}, {'RtpDataChannels': true }] };
-    // NOTE: createDataChannel needs `open /Applications/Google\ Chrome\ Canary.app --args --enable-data-channels` :-(
-    
-    var self = {
-        _id: 'peer-'+Math.random().toFixed(5).slice(2),
-        name: opts.name || "Friendly neighbor",
-        // TODO: see if WebRTC built-in identity provider stuff useful: http://www.ietf.org/proceedings/82/slides/rtcweb-13.pdf
-        identity: opts.identity || Math.random().toFixed(20).slice(2),
-        profile: opts.profile || {},
-        shares: Object.keys(opts.shares || {})
-    };
-    self.profile.browser = opts.browser || navigator.userAgent.replace(/^.*(Firefox|Chrome|Mobile)\/([0-9.]+).*$/, "$1 $2").replace("Mobile", "Bowser");
-    self[_t.presence] = true;
-    
-    function updateSelf(cb) {
-        if (updateSelf.againCallbacks) {
-            updateSelf.againCallbacks.push(cb);
-            return;
-        }
-        updateSelf.againCallbacks = [];
-        self.lastUpdate = new Date().toISOString();
-        hub.post(self, function (e,d) {
-            var againCallbacks = updateSelf.againCallbacks;
-            delete updateSelf.againCallbacks;
-            if (!e) self._rev = d.rev;
-            else console.warn("Trouble sharing presence", e, d);
-            call(cb, e, d);
-            if (againCallbacks.length) updateSelf(function (e,d) {
-                againCallbacks.forEach(function (cb) { call(cb,e,d); });
-            });
-        });
-    }
-    
-    var peers = Object.create(null);         // *connected* peers
-    
-    function associatedConnection(peer, initiatorCB) {
-        var peerInfo = peers[peer.identity];
-        if (!peerInfo) {
-            console.log(self.identity, "creating connection for", peer.identity);
-            peerInfo = peers[peer.identity] = {};
-            
-            // let code below use simple callback, but make sure all interested callers notified
-            peerInfo.callbacks = [initiatorCB];
-            var cb = function () {
-                var ctx = this, args = arguments;
-                peerInfo.callbacks.forEach(function (cb) { if (cb) cb.apply(ctx, args); });
-                delete peerInfo.callbacks;
-                cb = null;
-            };
-            
-            var rtc = peerInfo.connection = new RTCPeerConnection(cfg, con);
-            
-            var setupChannel = function (evt) {
-                if (evt) console.log(self.identity, "received data channel", evt.channel.readyState);
-                // NOTE: unreliable channel is not our preference, but that's all current FF/Chrome have
-                peerInfo.channel = (evt) ? evt.channel : rtc.createDataChannel('peerpouch-dev', {reliable:false});
-                peerInfo.channel.onopen = function (evt) {
-                    console.log(self.identity, "data channel is open");
-                    call(cb, null, peer);
-                };
-                peerInfo.channel.onmessage = function (evt) {
-                        console.log("Received message!", evt);
-                        receiveMessage(peer, JSON.parse(evt.data));
-                };
-            }
-            if (initiatorCB) setupChannel();
-            else rtc.ondatachannel = setupChannel;
-            
-            rtc.onnegotiationneeded = function (evt) {
-                console.log(self.identity, "saw negotiation trigger and will create an offer");
-                rtc.createOffer(function (offerDesc) {
-                        console.log(self.identity, "created offer, sending to", peer.identity);
-                        rtc.setLocalDescription(offerDesc);
-                        sendSignal(peer, _jsonclone(offerDesc));
-                }, function (e) { call(cb,e); });
-            };
-            rtc.onicecandidate = function (evt) {
-                if (evt.candidate) sendSignal(peer, {candidate:_jsonclone(evt.candidate)});
-            };
-            // debugging
-            rtc.onicechange = function (evt) {
-                console.log(self.identity, "ICE change", rtc.iceGatheringState, rtc.iceConnectionState);
-            };
-            rtc.onstatechange = function (evt) {
-                console.log(self.identity, "State change", rtc.signalingState, rtc.readyState)
-            };
-        } else if (peerInfo.callbacks) { 
-            peerInfo.callbacks.push(initiatorCB);
-        } else setTimeout(function () {
-            var e = (peerInfo.channel.readyState === 'open') ? null : Error("Connection exists, but data channel not open!");
-            call(initiatorCB, e);
-        }, 0);
-        return peerInfo.connection;
-    }
-    
-    // "messages" are peer-to-peer
-    function sendMessage(peer, data) {
-            var peerInfo = peers[peer.identity];
-            if (!peerInfo) throw Error("Not connected to peer!");
-            peerInfo.channel.send(JSON.stringify(data));
-    }
-    function receiveMessage(peer, data) {
-            if (data.type === 'rpc') {
-                    _rpc_recv(peer, data.fn, data.args);
-            }
-    }
-    
-    var _rpc_local = Object.create(null);
-    function _rpc_send(peer, fn, args) {
-        sendMessage(peer, {
-            type:'rpc', fn:fn,
-            args: JSON.stringify(args, function (k,v) {
-                if (typeof v === 'function') {
-                    var id = Pouch.uuid();
-                    // TODO: we need a cleanup strategy
-                    // https://github.com/TooTallNate/node-weak (node.js only)
-                    // http://wiki.ecmascript.org/doku.php?id=strawman:weak_refs (ES-never)
-                    // hacky idea: mark functions we expect to be called only once, assume heartbeat within a timeout for the rest?
-                    _rpc_local[id] = v;
-                    v = {__rpc:id};
-                }
-                return v;
-            })
-        });
-    }
-    function _rpc_recv(peer, fn, args) {
-        _rpc_local[fn].apply(null, JSON.parse(args, function (k,v) {
-            if (v.__rpc) {
-                var _rpc_id = v.__rpc;
-                v = function () {         // IMPORTANT: when this is GC'ed the remote should remove _rpc_id from its _rpc_local
-                    _rpc_send(peer, _rpc_id, arguments);
-                };
-            }
-            return v;
-        }));
-    }
-    
-    // TODO: implement this bootstrapping stuff somewhere
-    /*
-    _rpc_local['__db__'] = function (db, methods) {
-        // register (/unregister) locally
-    };
-    _rpc_send(peer, '__db__', ["dbname", {
-        get: function () {},
-        put: function () {}
-    }]);
-    */
-    
-    // "signals" are through centralized hub
-    function sendSignal(peer, data) {
-        var msg = {
-            sender: self.identity,
-            recipient: peer.identity,
-            data: data
-        };
-        msg[_t.signal] = true;
-        hub.post(msg, function (e) { if (e) throw e; });
-    }
-    function receiveSignal(peer, data) {
-        console.log(self.identity, "got", data, "from", peer.identity);
-        var rtc = associatedConnection(peer);
-        if (data.sdp) rtc.setRemoteDescription(new RTCSessionDescription(data), function () {
-            var needsAnswer = (rtc.remoteDescription.type == 'offer');
-            console.log(self.identity, "set offer, now creating answer:", needsAnswer);
-            if (needsAnswer) rtc.createAnswer(function (answerDesc) {
-                console.log(self.identity, "got anwer, sending back to", peer.identity);
-                rtc.setLocalDescription(answerDesc);
-                sendSignal(peer, _jsonclone(answerDesc));
-            }, function (e) { console.warn(self.identity, "couldn't create answer", e); });
-        }, function (e) { console.warn(self.identity, "couldn't set remote description", e) });
-        else if (data.candidate) rtc.addIceCandidate(new RTCIceCandidate(data.candidate));
-    }
-    
-    var changesListener;
-    function listenForChanges() {
-        var cancelListen = false;
-        changesListener = {
-            cancel: function () { cancelListen = true; }
-        };
-        hub.info(function (e,d) {
-            if (e) throw e;
-            var opts = {
-                //filter: _t.ddoc_name+'/signalling',             // see https://github.com/daleharvey/pouchdb/issues/525
-                include_docs: true,
-                continuous:true,
-                since:d.update_seq
-            };
-            opts.onChange = function (d) {
-                var doc = d.doc;
-                if (doc[_t.signal] && doc.recipient === self.identity) {
-                    receiveSignal({identity:doc.sender}, doc.data);
-                    // HACK: would hub.remove() but this is actually "simpler" due to https://github.com/daleharvey/pouchdb/issues/558
-                    hub.post({_id:doc._id,_rev:doc._rev,_deleted:true}, function (e) { if (e) throw e; });
-                } else if (doc[_t.presence] && doc.identity !== self.identity) {
-                    peersChanged(doc);
-                }
-            };
-            if (!cancelListen) changesListener = hub.changes(opts);
-            else changesListener = null;
-        });
-    }
-    
-    var api = {};
-    
-    // c.f. http://dev.w3.org/2011/webrtc/editor/webrtc.html#simple-peer-to-peer-example
-    // …and http://dev.w3.org/2011/webrtc/editor/webrtc.html#peer-to-peer-data-example
-    
-    // share our profile via hub
-    api.joinHub = function (cb) {
-        delete self._deleted;
-        updateSelf(cb);
-        listenForChanges();
-    };
-    
-    api.leaveHub = function (cb) {
-        self._deleted = true;
-        updateSelf(cb);
-        if (changesListener) changesListener.cancel();
-    };
-    
-    api.connectToPeer = associatedConnection;
-    
-    api.sendToPeer = sendMessage;
-    
-    // TODO: disconnectFromPeer
-    
-    var peerListeners = [];
-    function peersChanged(peer) {
-        peerListeners.forEach(function (cb) {
-            call(cb, peer);
-        });
-    }
-    
-    api.getPeers = function (opts, cb) {
-        if (typeof opts === 'function') {
-            cb = opts;
-            opts = {};
-        }
-        opts || (opts = {});
-        hub.query(_t.ddoc_name+'/peers_by_identity', {include_docs:true}, function (e, d) {
-            if (e) cb(e);
-            else cb(null, d.rows.filter(function (r) { return r.doc.identity !== self.identity; }).map(function (r) { return r.doc; }));
-        });
-        
-        var cancelListener = function () {};
-        if (opts.onChange) {            // WARNING/TODO: listener may get changes before cb returns initial list!
-            peerListeners.push(opts.onChange);
-            cancelListener = function () {
-                var cbIdx = peerListeners.indexOf(opts.onChange);
-                if (~cbIdx) peerListeners.splice(cbIdx, 1);
-            }
-        }
-        return {cancel:cancelListener};
-    };
-    
-    // TODO: is this really the best way to tackle changing client info?
-    api.updateInfo = function (newOpts, cb) {
-        // HACK: just update shares as needed for test page
-        Object.keys(newOpts).forEach(function (k) { opts[k] = newOpts[k]; });
-        self.shares = Object.keys(opts.shares || {});
-        updateSelf(cb);
-    };
-    
-    api.makeURL = function (peer, db) {
-        return "webrtc://" + peer.identity + '/' + db;
-    };
-    
-    if (!opts.nojoin) api.joinHub(cb);
-    
-    return api;
-};
-
-PeerPouch.Presence.verifyHub = function (hub, opts, cb) {
-    if (typeof opts === 'function') {
-        cb = opts;
-        opts = {};
-    }
-    hub.put(PeerPouch._ddoc, function (e,d) {
-        if (e && e.status === 409) {
-            console.warn("Found an existing design doc, proceeding with it…");
-            e = null;
-        }
-        // TODO: handle versioning (leave if higher minor, upgrade if lower minor, error if major difference)
-        call(cb, e, (e) ? null : {version:'dev'});
-    });
-}
-
-
-if (typeof module !== 'undefined' && module.exports) {
-    // running in node
-    var pouchdir = '../';
-    Pouch = require(pouchdir + 'pouch.js');
-    ajax = Pouch.utils.ajax;
-}
 
 // Register for our scheme
 Pouch.adapter('webrtc', PeerPouch);
@@ -576,7 +222,7 @@ var SharePouch = function (hub) {
                 opts.onChange = function (d) {
                     Object.keys(watchersByType).forEach(function (type) {
                         var watchers = watchersByType[type];
-                        if (d.doc[type] && watchers) watchers.forEach(function (cb) { call(cb, d.doc); });
+                        if (d.doc[type] && watchers) watchers.forEach(function (cb) { cb(d.doc); });
                     });
                 };
                 if (!cancelListen) changesListener = hub.changes(opts);
@@ -612,9 +258,9 @@ var SharePouch = function (hub) {
             info: opts.info || null
         };
         share[_t.share] = true;
-        hub.post(share, function (e, d) {
+        hub.post(share, function (e,d) {
             if (!e) share._rev = d.rev;
-            call(cb, e, d);
+            if (cb) cb(e,d);
         });
         
         var peerHandlers = Object.create(null);
@@ -646,6 +292,12 @@ var SharePouch = function (hub) {
         delete sharesByLocalId[db.id()];
     }
     
+    function _localShare(shareDoc) {
+        var name = [hub.id(),shareDoc._id].map(encodeURIComponent).join('/');
+        PeerPouch._sharePouches[name] = {hub:hub, peer:shareDoc._id};
+        return {dbname:'webrtc://'+name};
+    }
+    
     function getShares(opts, cb) {                    // TODO: wrap callbacks to set local .dbname property on each visible share
         if (typeof opts === 'function') {
             cb = opts;
@@ -655,15 +307,17 @@ var SharePouch = function (hub) {
         //hub.query(_t.ddoc_name+'/shares', {include_docs:true}, function (e, d) {
         hub.allDocs({include_docs:true}, function (e,d) {
             if (e) cb(e);
-            else cb(null, d.rows.filter(function (r) { return !(r.doc._id in sharesByRemoteId); }).map(function (r) { return r.doc; }));
+            else cb(null, d.rows.filter(function (r) { return !(r.doc._id in sharesByRemoteId); }).map(function (r) { return _localShare(r.doc); }));
         });
         if (opts.onChange) {            // WARNING/TODO: listener may get changes before cb returns initial list!
-            return addWatcher(_t.share, function (doc) { if (!(doc._id in sharesByRemoteId)) opts.onChange(doc); });
+            return addWatcher(_t.share, function (doc) { if (!(doc._id in sharesByRemoteId)) opts.onChange(_localShare(doc)); });
         }
     }
     
     return {shareDatabase:share, unshareDatabase:unshare, getSharedDatabases:getShares};
 }
+
+PeerPouch._sharePouches = Object.create(null);  // used to globally map share.dbname strings to hub/peer objects
 
 SharePouch._delete = function () {};            // blindly called by Pouch.destroy
 
